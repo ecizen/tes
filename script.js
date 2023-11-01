466901f69ee0f7fceba6f79981deed929bbf6360
ecizen <hardiektatendta@gmail.com>
2023-11-01 21:49:15 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,16 @@
+// let saldoAwal = prompt('masukan saldo awal')
+// let saldotambahan = prompt('masukan tambahan')
+// let Pajak= prompt('masukan pajak')
+
+// saldoAwal = parseFloat(saldoAwal);
+// saldotambahan = parseFloat(saldotambahan);
+// Pajak =  parseFloat(Pajak)
+
+
+// const saldoAkhir = saldoAwal + saldotambahan - Pajak
+// alert( `jumlah saldo anda adalah :${saldoAkhir}`)
+let hari = new Date().getDay()
+
+let Harinew = ['senin' , 'selasa', 'rabu', 'kamis', 'jumat', 'sabtu', 'minggu']
+
+console.log(`hari  ${Harinew}`)
